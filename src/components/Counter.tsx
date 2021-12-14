61daa417c39d619d7636d86434bd158b928e0340
BRUNO <bdelpalacior@fpcoruna.afundacion.org>
2021-12-14 13:08:43 +0100

    Ya se pueden aplicar estilos, al menos al contador :)

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,11 @@
 import { Button, Typography } from '@mui/material';
 import React, { useState } from 'react';
-
+import useStyles from './counter-styles';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
+    const classes = useStyles();
+
     const onDecreaseCount = () => {
         setCount(count - 1);
     };
@@ -12,11 +14,11 @@ const Counter = () => {
     };
     return (
         <div>
-            <Button onClick={onDecreaseCount} disabled={count === 0}>
+            <Button className={classes.button} onClick={onDecreaseCount} disabled={count === 0}>
                 -
             </Button>
             <Typography>{count}</Typography>
-            <Button onClick={onIncrementCount}>
+            <Button className={classes.button} onClick={onIncrementCount}>
                 +
             </Button>
 
---
+++ src/components/counter-styles.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles: any = makeStyles({
+    button: {
+        border: 'solid black 4px'
+    }
+});
+export default useStyles;
