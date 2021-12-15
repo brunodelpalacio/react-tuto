e1db18cf87a1db36b6bde512d52c6d33cc84aac8
BRUNO <bdelpalacior@fpcoruna.afundacion.org>
2021-12-15 13:54:28 +0100

    Ya se almacena el JSON de los productos.

--- src/screens/products/Products.tsx
+++ src/screens/products/Products.tsx
@@ -3,10 +3,11 @@ import { Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Counter from '../../components/Counter';
 import ProductsService from '../../services/ProducService';
+import { useState } from 'react';
 
 const Products = () => {
   const navigate = useNavigate();
-
+  const [products, setProducts] = useState([]);
   const onClickButton = () => {
     navigate('details');
   };
@@ -15,11 +16,13 @@ const Products = () => {
     async function asyncCall() {
       console.log('calling');
       const result = await ProductsService.getProducts();
+
+      setProducts(result.data);
     }
     asyncCall();
   }, []);
 
-  console.log(Products);
+  console.log(products);
 
   return (
     <div>
--- src/services/ProducService.ts
+++ src/services/ProducService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const ProductsService = {
     getProducts: () => {
 
-        return axios.get('https://raw.githubusercontent.com/RoiDopazo/react-tuto / develop / data.json');
+        return axios.get('https://raw.githubusercontent.com/RoiDopazo/react-tuto/develop/data.json');
 
     }
 };
